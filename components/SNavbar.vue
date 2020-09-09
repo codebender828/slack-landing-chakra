bd99a71a04354061536f772134d82767f2f4cb55
codebender828 <jbakebwa@163.com>
2020-09-09 22:13:41 +0800

    chore: cleanup navigation.

--- components/SNavbar.vue
+++ components/SNavbar.vue
@@ -2,15 +2,21 @@
   <c-box as="nav" bg="white" shadow="lg" z-index="1">
     <c-flex p="5" max-w="80rem" mx="auto" align="center">
       <s-logo />
-      <s-nav-content
-        :display="['none', 'none', 'flex']"
-        ml="5"
-        w="full"
+      <c-box
         align="center"
-        spacing="6"
-        is-inline
-      />
-      <s-mobile-nav ml="auto" :display="['flex', 'flex', 'none']" />
+        mr="auto"
+        width="100%"
+        ml="5"
+        :d="['none', 'none', 'flex']"
+      >
+        <s-nav-content
+          w="full"
+          align="center"
+          spacing="6"
+          is-inline
+        />
+      </c-box>
+      <s-mobile-nav ml="auto" :d="['flex', 'flex', 'none']" />
     </c-flex>
   </c-box>
 </template>
